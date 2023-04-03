8d110a69ab70f56a2c6268d68c0c2612b7d16a1d
Edvard <Edvard@KOMPKRASAVCIKA>
2023-04-03 05:04:13 +0400

    Add project files.

---
+++ Sortr1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Concurrent;
+
+namespace Sortr1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {//For Integers
+                Console.WriteLine("Enter a number of integers : ");
+                int max = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter numbers : ");
+                int[] numbers = new int[max];
+
+                for (int i = 0; i < max; i++)
+                {
+                    numbers[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                SortrForInt.QuickSort(numbers, 0, max - 1);
+
+                Console.WriteLine("Here are your values : ");
+
+                for (int i = 0; i < max; i++)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+
+
+            {//For Chars
+                Console.WriteLine("Enter a number of letters: ");
+                int max = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter a letter : ");
+                string notSortedWord = Console.ReadLine();
+                var letters = notSortedWord.ToCharArray();
+
+
+                char[] chars = new char[max];
+
+                for (int i = 0; i < max; i++)
+                {
+                    chars[i] = Convert.ToChar(Console.ReadLine());
+                }
+
+                SortrForChar.QuickSort(chars, 0, max - 1);
+
+                Console.WriteLine("Here are your values : ");
+
+                for (int i = 0; i < max; i++)
+                {
+                    Console.Write(chars[i] + " ");
+                }
+            }
+
+
+            {//For Double
+                Console.WriteLine("Enter a number of decimal numbers : ");
+                int max = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter decimal numbers : ");
+                double[] numbers = new double[max];
+
+                for (int i = 0; i < max; i++)
+                {
+                    numbers[i] = Convert.ToDouble(Console.ReadLine());
+                }
+
+                SortrForDouble.QuickSort(numbers, 0, max - 1);
+
+                Console.WriteLine("Here are your values : ");
+
+                for (int i = 0; i < max; i++)
+                {
+                    Console.Write(numbers[i] + "   ");
+                }
+            }
+
+
+        }
+
+    }
+}
---
+++ Sortr1/Sortr.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sortr1
+{
+    public class SortrForInt 
+    {
+        public static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            int pivotIndex = GetPivotIndex(array, minIndex, maxIndex);
+
+            QuickSort(array, minIndex, pivotIndex - 1);
+
+            QuickSort(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+        public static int GetPivotIndex(int[] array, int minIndex, int maxIndex)
+        {
+            int pivot = minIndex - 1;
+
+            for (int i = minIndex; i <= maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+
+            return pivot;
+        }
+        public static void Swap(ref int leftItem, ref int rightItem)
+        {
+            int temp = leftItem;
+
+            leftItem = rightItem;
+
+            rightItem = temp;
+        }
+    }
+
+    public class SortrForChar
+    {
+        public static char[] QuickSort(char[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            int pivotIndex = GetPivotIndex(array, minIndex, maxIndex);
+
+            QuickSort(array, minIndex, pivotIndex - 1);
+
+            QuickSort(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+        public static int GetPivotIndex(char[] array, int minIndex, int maxIndex)
+        {
+            int pivot = minIndex - 1;
+
+            for (int i = minIndex; i <= maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+
+            return pivot;
+        }
+        public static void Swap(ref char leftItem, ref char rightItem)
+        {
+            char temp = leftItem;
+
+            leftItem = rightItem;
+
+            rightItem = temp;
+        }
+    }
+
+    public class SortrForDouble
+    {
+        public static double[] QuickSort(double[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            int pivotIndex = GetPivotIndex(array, minIndex, maxIndex);
+
+            QuickSort(array, minIndex, pivotIndex - 1);
+
+            QuickSort(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+        public static int GetPivotIndex(double[] array, int minIndex, int maxIndex)
+        {
+            int pivot = minIndex - 1;
+
+            for (int i = minIndex; i <= maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+
+            return pivot;
+        }
+        public static void Swap(ref double leftItem, ref double rightItem)
+        {
+            double temp = leftItem;
+
+            leftItem = rightItem;
+
+            rightItem = temp;
+        }
+    }
+}
